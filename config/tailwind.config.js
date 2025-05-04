8e60bbdc0e5296c81634d028ffb64fcecbe6d5b6
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-05-04 17:08:04 +0900

    twdのアップグレード

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,21 +1,20 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+// // const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    '../public/*.html',
+    '../app/helpers/**/*.rb',
+    '../app/javascript/**/*.js',
+    '../app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+// //         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [
-    require('daisyui'),
     // require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
     // require('@tailwindcss/container-queries'),
